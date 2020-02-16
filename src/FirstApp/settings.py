58400cc7752032fb3b4c5e18ca756d9821da08a5
Lukas Fiedler <fiedlerlukas@gmail.com>
2020-02-16 22:49:31 +0100

    part 2 done

--- src/FirstApp/settings.py
+++ src/FirstApp/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #OWN
+    'polls.apps.PollsConfig',
+
+    #DEFAULT
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
